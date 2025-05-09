21ec1a61b0d8c3715b92c0bd343dde52c6fd34e7
Leon Schumacher <leonsch@protonmail.com>
2025-05-09 18:19:29 +0200

    pull sbctl from fresh, since aquaris needs at least v0.15

--- rice/patches.nix
+++ rice/patches.nix
@@ -7,9 +7,15 @@ in
 {
   nixpkgs.overlays = [
     (_:_: {
-      # jujutsu in our default input (nixos-24.11) is version 0.23.0
-      # which is marked as insecure (GHSA-794x-2rpg-rfgr)
-      inherit (pkgs-fresh) jujutsu;
+
+      inherit (pkgs-fresh)
+        # jujutsu in our default input (nixos-24.11) is version 0.23.0
+        # which is marked as insecure (GHSA-794x-2rpg-rfgr)
+        jujutsu
+
+        # aquaris needs version 0.15 of sbctl, which supports --config
+        sbctl
+        ;
     })
   ];
 }
