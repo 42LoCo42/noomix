10960e1ece7b93307085109845db722fe3709ae6
Leon Schumacher <leonsch@protonmail.com>
2025-10-04 16:29:58 +0200

    Update Aquaris to fix 25.05 errors

--- flake.nix
+++ flake.nix
@@ -1,18 +1,26 @@
 {
   inputs = {
-    aquaris.url = "github:42loco42/aquaris";
-    aquaris.inputs.home-manager.follows = "home-manager";
-    aquaris.inputs.nixpkgs.follows = "nixpkgs";
-    aquaris.inputs.obscura.follows = "obscura";
+    aquaris = {
+      url = "github:42loco42/aquaris";
+      inputs = {
+        home-manager.follows = "home-manager";
+        nixpkgs.follows = "nixpkgs";
+        obscura.follows = "obscura";
+      };
+    };
 
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-fresh.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    nur.url = "github:nix-community/nur";
-    nur.inputs.nixpkgs.follows = "nixpkgs";
+    nur = {
+      url = "github:nix-community/nur";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     obscura.url = "github:42loco42/obscura";
   };
